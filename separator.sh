9f77f913502c8296e13c9a7f6a6946376d90a1c5
Soumya Mandi <soumyamandi@gmail.com>
2012-10-16 14:32:20 +0530

    first commit

---
+++ separator.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+
+#for Fut vs Fut
+grep -v Lum | grep -v Sec > FF_scores.txt
+
+#for Fut vs Lum
+
+
+#for Fut vs Sec
+
+
+#for Lum vs Fut
+
+#for Lum vs Lum
+
+#for Lum vs Sec
+
+#for Sec vs Fut
+
+#for Sec vs Lum
+
+#for Sec vs Sec
